38df41dfe6a54ae4424bf828c5e9135242c02755
TheMoon <woghd1008@gmail.com>
2026-04-21 22:16:00 +0900

    2026-04-21 Java Hash

---
+++ src/collection/set/MyHashSetV0Main.java
@@ -0,0 +1,22 @@
+package collection.set;
+
+public class MyHashSetV0Main {
+
+    public static void main(String[] args) {
+        MyHashSetV0 set = new MyHashSetV0();
+        set.add(1); //0(1)
+        set.add(2); //0(n)
+        set.add(3); //0(n)
+        set.add(4); //0(n)
+        set.add(5); //0(n)
+        System.out.println(set);
+
+        boolean result = set.add(4);
+        System.out.println("중복 데이터 저장 결과 = " + result);
+        System.out.println(set);
+
+        System.out.println("set.constains(3) = " + set.contains(3));
+        System.out.println("set.constains(99) = " + set.contains(99));
+
+    }
+}
