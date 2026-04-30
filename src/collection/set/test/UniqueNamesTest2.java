4f958c8a0b4f4e320457846de1eb2fa7f9defcfc
TheMoon <woghd1008@gmail.com>
2026-04-30 21:14:23 +0900

    2026-04-30 Java Set

---
+++ src/collection/set/test/UniqueNamesTest2.java
@@ -0,0 +1,23 @@
+package collection.set.test;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UniqueNamesTest2 {
+
+    public static void main(String[] args) {
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+        Set<Integer> set = new LinkedHashSet<>(List.of(inputArr)); //0(1)
+
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = List.of(1, 2, 3);
+        for (Integer s : set) {
+            System.out.println(s);
+        }
+    }
+
+
+
+}
