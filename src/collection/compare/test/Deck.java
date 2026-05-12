88ef239c7b69d63c90aef99d3b37372a09a05b14
TheMoon <woghd1008@gmail.com>
2026-05-12 22:20:04 +0900

    2026-05-12 Java Iteralbe, Comparable, Collections

---
+++ src/collection/compare/test/Deck.java
@@ -0,0 +1,34 @@
+package collection.compare.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.shuffle;
+
+public class Deck {
+
+    private List<Card> cards = new ArrayList<>();
+
+    public Deck() {
+        initCard();
+        shuffle();
+
+    }
+
+    private void initCard() {
+        for (int i = 1; i < 13; i++) {
+            for (Suit suit : Suit.values()) {
+                cards.add(new Card(i, suit));
+            }
+        }
+    }
+
+    private void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        return cards.remove(0);
+    }
+}
